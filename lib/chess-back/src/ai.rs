4e33ffb04bd31b4159da1f6fcbc000fa7c3b352d
martin.madier <martin.madier@epita.fr>
2025-02-13 14:15:44 +0100

    Refonte jeu

---
+++ lib/chess-back/src/ai.rs
@@ -0,0 +1,23 @@
+enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+}
+
+struct AI {
+    difficulty: Difficulty,
+}
+
+impl AI {
+    fn new(difficulty: Difficulty) -> Self {
+        AI { difficulty }
+    }
+
+    fn get_best_move(&self, board: &Board) -> (Position, Position) {
+        (Position::new(0, 0), Position::new(1, 0))
+    }
+
+    fn evaluate_board(&self, board: &Board) -> i32 {
+        0
+    }
+}
