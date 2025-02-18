e93512bec775d666504d10804358f22ccd4d2cee
MatthisDev <matthisguillet71@gmail.com>
2025-02-18 12:32:26 +0100

    add test lib and remove unused and dead_code flags

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod game_lib;
+
+// define the folder <tests> as testing folder
+#[cfg(test)]
+mod tests;
---
+++ src/tests/t_game_lib/game.rs
@@ -0,0 +1,26 @@
+use crate::game_lib::game::Game;
+
+#[test]
+fn t_game() {
+    let mut game = Game::init();
+    game.board.update_attack_info();
+    game.board.print_attack_info();
+    game.board.print_board();
+
+    // Effectuer un mouvement
+    if let Ok(_) = game.make_move_algebraic("e2", "e4") {
+        game.board.print_board();
+    }
+    println!("turn:{:?}", game.board.turn);
+    if let Ok(_) = game.make_move_algebraic("d7", "d5") {
+        game.board.print_board();
+    }
+    println!("turn:{:?}", game.board.turn);
+    if let Ok(_) = game.make_move_algebraic("e4", "d5") {
+        game.board.print_board();
+    }
+    println!("turn:{:?}", game.board.turn);
+    if let Ok(_) = game.make_move_algebraic("d8", "d5") {
+        game.board.print_board();
+    }
+}
