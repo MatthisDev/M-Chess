7d23794bcf2f567fdb5da4ce183ab1067c571fc6
Matteo <matteo.wermert2@gmail.com>
2025-02-20 15:06:59 +0100

    pieces

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,12 +1,31 @@
 use yew::prelude::*;
 
 enum Msg {
-    AddOne,
+    SelectPiece(usize),
+    MovePiece(usize),
+}
+
+#[derive(Clone, Copy)]
+enum Piece {
+    WhitePawn,
+    BlackPawn,
+    WhiteRook,
+    BlackRook,
+    WhiteKnight,
+    BlackKnight,
+    WhiteBishop,
+    BlackBishop,
+    WhiteQueen,
+    BlackQueen,
+    WhiteKing,
+    BlackKing,
 }
 
 struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    board: [Option<Piece>; 64],
+    selected: Option<usize>,
+    last_move: Option<(usize, usize)>,
 }
 
 impl Component for Model {
@@ -14,13 +33,49 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        let mut board = [None; 64];
+        // Initialize pawns
+        for i in 8..16 {
+            board[i] = Some(Piece::WhitePawn);
+            board[40 + i] = Some(Piece::BlackPawn);
+        }
+        // Initialize other pieces
+        board[0] = Some(Piece::WhiteRook);
+        board[1] = Some(Piece::WhiteKnight);
+        board[2] = Some(Piece::WhiteBishop);
+        board[3] = Some(Piece::WhiteQueen);
+        board[4] = Some(Piece::WhiteKing);
+        board[5] = Some(Piece::WhiteBishop);
+        board[6] = Some(Piece::WhiteKnight);
+        board[7] = Some(Piece::WhiteRook);
+
+        board[56] = Some(Piece::BlackRook);
+        board[57] = Some(Piece::BlackKnight);
+        board[58] = Some(Piece::BlackBishop);
+        board[59] = Some(Piece::BlackQueen);
+        board[60] = Some(Piece::BlackKing);
+        board[61] = Some(Piece::BlackBishop);
+        board[62] = Some(Piece::BlackKnight);
+        board[63] = Some(Piece::BlackRook);
+
+        Self { link, board, selected: None, last_move: None }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
+            Msg::SelectPiece(index) => {
+                self.selected = Some(index);
+                true
+            }
+            Msg::MovePiece(index) => {
+                if let Some(selected) = self.selected {
+                    if selected != index {
+                        self.board[index] = self.board[selected];
+                        self.board[selected] = None;
+                        self.last_move = Some((selected, index));
+                        self.selected = None;
+                    }
+                }
                 true
             }
         }
@@ -33,9 +88,8 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
                 { self.chessboard() }
+                { self.view_last_move() }
             </div>
         }
     }
@@ -49,13 +103,49 @@ impl Model {
             } else {
                 "black"
             };
-            html! { <div class={format!("square {}", class)}></div> }
+            let piece = match self.board[i] {
+                Some(Piece::WhitePawn) => "♙",
+                Some(Piece::BlackPawn) => "♟︎",
+                Some(Piece::WhiteRook) => "♖",
+                Some(Piece::BlackRook) => "♜",
+                Some(Piece::WhiteKnight) => "♘",
+                Some(Piece::BlackKnight) => "♞",
+                Some(Piece::WhiteBishop) => "♗",
+                Some(Piece::BlackBishop) => "♝",
+                Some(Piece::WhiteQueen) => "♕",
+                Some(Piece::BlackQueen) => "♛",
+                Some(Piece::WhiteKing) => "♔",
+                Some(Piece::BlackKing) => "♚",
+                None => "",
+            };
+            let onclick = if self.selected.is_some() {
+                self.link.callback(move |_| Msg::MovePiece(i))
+            } else {
+                self.link.callback(move |_| Msg::SelectPiece(i))
+            };
+            html! {
+                <div class={format!("square {}", class)} onclick=onclick>
+                    { piece }
+                </div>
+            }
         });
 
         html! {
             <div class="chessboard">
                 { for squares }
             </div>
+        }
+    }
+
+    fn view_last_move(&self) -> Html {
+        if let Some((from, to)) = self.last_move {
+            let from_coord = format!("{}{}", (from % 8 + 97) as u8 as char, 8 - from / 8);
+            let to_coord = format!("{}{}", (to % 8 + 97) as u8 as char, 8 - to / 8);
+            html! {
+                <p>{ format!("Last move: {} -> {}", from_coord, to_coord) }</p>
+            }
+        } else {
+            html! { <p>{ "No moves yet" }</p> }
         }
     }
 }
